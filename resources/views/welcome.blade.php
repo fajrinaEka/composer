66b2dbbc3d6778d64547e8bdc347ef08b6fa39fa
fajrinaEka <fajrinabella@gmail.com>
2021-10-27 13:01:59 +0700

    composer

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fajrina</title>
+
+	<style>
+		body{
+			background-image: url(https://i.pinimg.com/564x/67/f1/2d/67f12dcac5eb8a663e089acca5c6b75c.jpg);
+			background-size: cover;
+			background-position: top;
+			<background-repeat: no-repeat;
+
+
+	    
+
+		}
+	
+.container{ 
+	position: absolute;
+	 left: 50%; 
+	 top: 50%; 
+	 transform: translate(-50%,-50%); 
+	 padding: 20px 25px; 
+	 width: 300px; 
+	 background-color: rgba(0,0,0,.7);
+	  box-shadow: 0 0 10px rgba(255,255,255,.3);}
+		}
+		</style>
+
+
+	
+</head>
+<body>
+	<center>
+		<div class="container">
+	<div class="login-boxt">
+		<font color="white"><h1>Login</h1></font>
+		<div class="textbox">
+			<i class="fas fa-user"></i>
+			<input type="text" placeholder="Username">
+		</div>
+
+	<div class="textbox">
+		<i class="fas fa-lock"></i>
+		<input type="password" placeholder="password">
+	</div>
+	<a href="https://www.google.com/"><input type="button" clas="btn" value="Login"></a>
+
+	
+	
+    </div>
+	</div>
+	</center>
+
+	
+</body>
+</html>
